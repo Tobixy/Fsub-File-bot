8a68ecb69e4a113ebe558643e533bbe101ec6038
Tobixy <shivamrajput69bs@gmail.com>
2023-09-18 11:27:16 +0530

    Create fsub.py

---
+++ fsub.py
@@ -0,0 +1,3 @@
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001598773095"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "1001843842798"))
